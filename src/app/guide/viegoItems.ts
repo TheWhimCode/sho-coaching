0bb9e0526262b9b79e78c881b4e6b1d7b51da893
TheWhimCode <im.sho@yahoo.com>
2026-06-28 14:42:04 +0200

    guide greensmite hide

--- src/app/guide/viegoItems.ts
+++ src/app/guide/viegoItems.ts
@@ -130,10 +130,6 @@ const VIEGO_PRE_BUILD: GuideItemSectionConfig["preBuild"] = {
         "Since you're building scaling, you lack skirmishing power early levels. Wards help you deal with those early invades and smoothen the game when you crossmap. Swap to sweeper when you're strong enough to oneshot",
     },
   ],
-  startingLink: {
-    label: "Green Smite TikTok",
-    href: "https://www.tiktok.com/@itsMinooooo",
-  },
   bootsBase: {
     id: 1001,
     title: "Boots",
